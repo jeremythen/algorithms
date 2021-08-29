288ec0df3e3ff83989dfe0a8bd34ce3c51e8113f
Jeremy Then <jeremythen16@gmail.com>
2021-08-28 20:10:48 -0400

    Grid Traveler, 3rd version

--- Numbers/Grid_Traveler/gridTraveler.js
+++ Numbers/Grid_Traveler/gridTraveler.js
@@ -1,11 +1,11 @@
-function grid(n1, n2) {
-    if (n1 === 0 || n2 === 0) {
+function grid(n, m) {
+    if (n === 0 || m === 0) {
         return 0;
     }
-    if (n1 === 1 && n2 === 1) {
+    if (n === 1 && m === 1) {
         return 1;
     }
-    return grid(n1 - 1, n2) + grid(n1, n2 - 1);
+    return grid(n - 1, m) + grid(n, m - 1);
 }
 
 const result = grid(3, 3);
