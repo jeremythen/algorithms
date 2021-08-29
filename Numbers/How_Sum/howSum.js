821f9deb78b6a90be2b79ef0f22415bed8385e6e
Jeremy Then <jeremythen16@gmail.com>
2021-08-28 23:12:29 -0400

    Updates How Sum. Adds memo. 2nd version.

--- Numbers/How_Sum/howSum.js
+++ Numbers/How_Sum/howSum.js
@@ -1,19 +1,27 @@
 function howSum(target, numbers) {
-    if (target === 0) {
-        return [];
-    }
-    if (target < 0) {
-        return null;
-    }
-    for (let i = 0; i < numbers.length; i++) {
-        const num = numbers[i];
-        const newTarget = target - num;
-        const result = howSum(newTarget, numbers);
-        if (result) {
-            return [...result, num];
+    const cache = new Map();
+    const howSumClosure = (target) => {
+        if (target === 0) {
+            return [];
         }
-    }
-    return null;
+        if (target < 0) {
+            return null;
+        }
+        if (cache.has(target)) {
+            return cache.get(target);
+        }
+        for (let i = 0; i < numbers.length; i++) {
+            const num = numbers[i];
+            const newTarget = target - num;
+            const result = howSum(newTarget, numbers);
+            cache.set(newTarget, result);
+            if (result) {
+                return [...result, num];
+            }
+        }
+        return null;
+    };
+    return howSumClosure(target);
 }
 
 console.log(howSum(7, [2, 4]));
