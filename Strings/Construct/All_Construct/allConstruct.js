485e4582ecef0f63077694fc08417601c46aa667
Jeremy Then <jeremythen16@gmail.com>
2021-08-29 16:23:17 -0400

    Updates All_Construct. Adds cache. 2nd version

--- Strings/Construct/All_Construct/allConstruct.js
+++ Strings/Construct/All_Construct/allConstruct.js
@@ -1,14 +1,18 @@
 
-function allConstruct(target, wordBank) {
+function allConstruct(target, wordBank, cache = new Map()) {
     if (target === '') {
         return [[]];
+    }
+    if (cache.has(target)) {
+        return cache.get(target);
     }
     let arr = [];
     for (let i = 0; i < wordBank.length; i++) {
         const word = wordBank[i];
         if (target.indexOf(word) === 0) {
             const subStr = target.slice(word.length);
-            const result = allConstruct(subStr, wordBank);
+            const result = allConstruct(subStr, wordBank, cache);
+            cache.set(target, result);
             const resultMapped = result.map(values => [word, ...values]);
             arr.push(...resultMapped);
         }
