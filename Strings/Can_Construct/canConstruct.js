c0bd3ed1651020b1ada3d45ec5d8ce3fd89aeef2
Jeremy Then <jeremythen16@gmail.com>
2021-08-29 00:52:33 -0400

    Updates Can_Construct. Adds memo. 2nd version.

--- Strings/Can_Construct/canConstruct.js
+++ Strings/Can_Construct/canConstruct.js
@@ -1,10 +1,14 @@
 
 function canConstruct(target, wordBank) {
     const wordBankCache = new Set();
+    const cache = new Map();
     wordBank.forEach(word => wordBankCache.add(word));
     const canConstructClosure = (str) => {
         if (str === '') {
             return true;
+        }
+        if (cache.has(str)) {
+            return cache.get(str);
         }
         if (wordBankCache.has(str)) {
             return true;
@@ -14,6 +18,8 @@ function canConstruct(target, wordBank) {
             const subStr2 = str.substr(i, str.length - 1);
             const result1 = canConstructClosure(subStr1);
             const result2 = canConstructClosure(subStr2);
+            cache.set(subStr1, result1);
+            cache.set(subStr2, result2);
             if (result1 && result2) {
                 return true;
             }
