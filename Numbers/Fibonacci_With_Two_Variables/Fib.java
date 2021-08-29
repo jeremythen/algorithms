67bf6a4e7acbe744701e4b96da98a65090f4486a
Jeremy Then <jeremythen16@gmail.com>
2021-08-29 18:27:32 -0400

    Adds Fibonacci_With_Two_Variables in java.

---
+++ Numbers/Fibonacci_With_Two_Variables/Fib.java
@@ -0,0 +1,19 @@
+public class Fib {
+    public static void main(String... args) {
+
+        System.out.println(fib(6) == 8);
+        System.out.println(fib(7) == 13);
+        System.out.println(fib(8) == 21);
+
+    }
+
+    public static int fib(int n) {
+        int v1 = 0;
+        int v2 = 1;
+        for (int i = 1; i < n; i++) {
+            v2 = v1 + v2;
+            v1 = v2 - v1;
+        }
+        return v2;
+    }
+}
