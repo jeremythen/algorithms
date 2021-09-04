54159b53d5cccd2885014d993a083140daf56f47
Jerermy.Then <jerermy.then@nordstrom.com>
2021-09-03 22:52:26 -0400

    Adds rowSumOddNumbers.java

---
+++ Numbers/Row_Sum_Odd_Numbers/RowSumOddNumbers.java
@@ -0,0 +1,13 @@
+public class RowSumOddNumbers {
+
+    public static void main(String... args) {
+        System.out.println(rowSumOddNumbers(3) == 27);
+        System.out.println(rowSumOddNumbers(4) == 64);
+        System.out.println(rowSumOddNumbers(5) == 125);
+    }
+
+    public static int rowSumOddNumbers(int n) {
+        return (int)Math.pow(n, 3);
+    }
+    
+}
