04fe0ec1b389764509d920d09d93bf301096ffe2
Jeremy Then <jeremythen16@gmail.com>
2021-09-14 00:15:21 -0400

    Updates removeDuplicates.js, adds most efficient solution.

--- Numbers/Remove_Duplicates/removeDuplicates.js
+++ Numbers/Remove_Duplicates/removeDuplicates.js
@@ -13,6 +13,19 @@ function solution(numbers) {
     return i;
 }
 
+
+// Most efficient solution.
+function solution2(numbers) {
+    let i = 0;
+    for (let j = 1; j < numbers.length; j++) {
+        if (numbers[i] === numbers[j]) {
+        } else {
+            numbers[++i] = numbers[j];
+        }
+    }
+    return ++i;
+}
+
 solution([1, 1, 2]);
 solution([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
 
