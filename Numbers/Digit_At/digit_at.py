96e65dbee3042e247a38d201c05bcd3664489d11
Jeremy Then <jeremythen16@gmail.com>
2021-09-17 20:10:45 -0400

    Adds digit_at.py

---
+++ Numbers/Digit_At/digit_at.py
@@ -0,0 +1,12 @@
+
+import math
+
+
+def digit_at(number, index):
+    return math.floor(number / 10 ** (index - 1) % 10)
+
+
+print(digit_at(1234567, 1) == 7)
+print(digit_at(1234567, 2) == 6)
+print(digit_at(1234567, 3) == 5)
+print(digit_at(1234567, 4) == 4)
